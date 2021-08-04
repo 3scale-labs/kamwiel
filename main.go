d765d3e45d2f8786e686c6794fb91f4786874502
dd di cesare <didi@posteo.net>
2021-08-04 10:34:51 +0700

    [main] Triggering gingonic in a go routine and starting k8s controllers

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/3scale-labs/kamwiel/pkg/routing"
+import (
+	"github.com/3scale-labs/kamwiel/controllers"
+	"github.com/3scale-labs/kamwiel/pkg/routing"
+)
 
 func main() {
-	routing.Start()
+	go routing.Start()
+	controllers.Start()
 }
